6fe1325dd1019992e012f138dd1722681ca50199
Lalit Panwar <41753714+hppy97@users.noreply.github.com>
2021-07-27 20:49:28 +0530

    Create demo.js

---
+++ JS/demo.js
@@ -0,0 +1,25 @@
+var i=10;
+let x=5;
+
+console.log(i)
+console.log(x)
+
+{
+    i=20;
+    var q=40;
+}
+console.log(q)
+q=26;
+console.log(q)
+
+{
+    let x=30;
+    console.log(x)
+}
+console.log(i)
+console.log(x)
+
+
+let a = "5";      
+let b= + a;
+console.log(b)  
