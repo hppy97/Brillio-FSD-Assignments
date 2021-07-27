2b0691513729474ea090ccd47e202dc907507597
Lalit Panwar <41753714+hppy97@users.noreply.github.com>
2021-07-27 20:50:36 +0530

    Add files via upload

---
+++ JS/Logical.js
@@ -0,0 +1,16 @@
+let x=10;
+let y=5;
+
+if(x<15 && y>0)
+console.log("True");
+
+
+if(x==10 || y==2)
+console.log("True");
+
+
+if(!(x==y))
+console.log("True");
+
+var age=22;
+console.log((age >= 18) ? "Eligible":"Not eligible");
